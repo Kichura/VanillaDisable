d51c6d3c761348bc3d621f138c5aaa882e7ecfc1
DragonEggBedrockBreaking <68545280+DragonEggBedrockBreaking@users.noreply.github.com>
2024-09-15 19:43:43 +0100

    Fix entering other dimensions taking ages (don't resend commands to client unnecessarily)

---
+++ common/src/main/java/uk/debb/vanilla_disable/mixin/util/MixinPlayerList.java
@@ -0,0 +1,23 @@
+package uk.debb.vanilla_disable.mixin.util;
+
+import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
+import net.minecraft.commands.Commands;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.server.players.PlayerList;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(PlayerList.class)
+public abstract class MixinPlayerList {
+    @WrapWithCondition(
+            method = "sendPlayerPermissionLevel(Lnet/minecraft/server/level/ServerPlayer;I)V",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/commands/Commands;sendCommands(Lnet/minecraft/server/level/ServerPlayer;)V"
+            )
+    )
+    private boolean vanillaDisable$sendCommands(Commands instance, ServerPlayer player) {
+        String placementMethod = Thread.currentThread().getStackTrace()[4].getMethodName();
+        return !(placementMethod.equals("changeDimension") || placementMethod.equals("method_5731"));
+    }
+}

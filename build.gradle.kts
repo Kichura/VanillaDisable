e9ec7e32018395c513b02a1057aab8d81c4176dc
DragonEggBedrockBreaking <68545280+DragonEggBedrockBreaking@users.noreply.github.com>
2024-10-28 13:46:43 +0000

    Bump asm (9.7 -> 9.7.1)

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ val MINECRAFT_VERSION by extra { "1.21.1" }
 val MINECRAFT_VERSION_RANGE_FABRIC by extra { ">=1.21" }
 val MINECRAFT_VERSION_RANGE_NEOFORGE by extra { "[1.21,1.22)" }
 val H2_VERSION by extra { "2.3.232" }
-val ASM_VERSION by extra { "9.7" }
+val ASM_VERSION by extra { "9.7.1" }
 val PARCHMENT_MC_VERSION by extra { "1.21" }
 val PARCHMENT_VERSION by extra { "2024.07.28" }
 val MIXIN_EXTRAS_VERSION by extra { "0.4.1" }
